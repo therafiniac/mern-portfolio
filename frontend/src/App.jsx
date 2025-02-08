cc579932cb168df6c65c443660d07b5fb76b77dc
therafiniac <therafiniac@gmail.com>
2025-02-08 18:26:10 +0530

    Install react and create navbar by react router

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
+import { Navbar } from "./components/Navbar";
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { NavLink } from "react-router-dom";
+export const Navbar = () => {
+  return (
+    <>
+      <header>
+        <div className="container">
+          <div className="logo-brand">
+            <NavLink href="http://">Portfolio</NavLink>
+          </div>
+
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">About</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact">Contact</NavLink>
+              </li>
+              <li>
+                <NavLink to="/Register">Register</NavLink>
+              </li>
+              <li>
+                <NavLink to="/login">Login</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+};
